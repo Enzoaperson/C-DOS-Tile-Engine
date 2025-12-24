87c76e357264e1af3f0e29115c8cef92506c4d3d
Enzo <enzomlg88@gmail.com>
2025-12-24 10:35:04 -0800

    Updated to support 32-bit sizes! (Not sure if that'll actually be helpful, but idk)

--- src/asm.h
+++ src/asm.h
@@ -2,11 +2,11 @@
 #define ASM
 
 
-int memcpy_transparent(void *to, const void *from, char length);
+int memcpy_transparent(void *to, const void *from, int length);
 #pragma aux memcpy_transparent =  \
 				  "mov ecx,0xFFFFFFFF" \
 				  "draw_loop: inc ecx" \
-				  "cmp cl,dl" \
+				  "cmp ecx,esi" \
 				  "je draw_loop_end" \
 				  "mov dh,[ebx+ecx]" \
 				  "test dh,dh" \
@@ -14,21 +14,7 @@ int memcpy_transparent(void *to, const void *from, char length);
 				  "mov [eax+ecx],dh" \
 				  "jmp draw_loop" \
 			          "draw_loop_end: nop" \
-				  modify [ebx ecx edx dh esi] parm [eax] [ebx] [dl] value [ecx];
+				  modify [ebx ecx edx esi] parm [eax] [ebx] [esi] value [ecx];
 
-/*
-int memcpy_transparent(void *to, const void *from, short length);
-#pragma aux memcpy_transparent = \
-				 "	     mov ecx, 0xFFFF   " \
-				 "draw_loop: inc ecx           " \
-				 "           cmp cx,bx         " \
-				 "           je draw_loop_end  " \
-				 "           mov dh,[esi+ecx]  " \
-                                 "           test dh,dh        " \
-				 "           jz draw_loop      " \
-				 "           mov [edi+ecx],dh  " \
-				 "           jmp draw_loop     " \
-				 "draw_loop_end:               " \
-				modify [esi edi ecx bx dh] parm [edi] [esi] [bx] value [EDI];
-*/
+
 #endif
